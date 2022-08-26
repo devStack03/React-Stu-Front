f9b59a6ab0f6037bd2601c7044fa47ef32b0567b
devStack03 <devking0325@gmail.com>
2022-08-26 11:01:51 +0900

    initial project structure setup

---
+++ src/Layout/public.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchHeader from "../components/SearchHeader/SearchHeader";
+import CommonHeader from "../components/Header/CommonHeader";
+
+class PublicLayout extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <CommonHeader></CommonHeader>
+        <SearchHeader></SearchHeader>
+        <main>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
+export default PublicLayout;
